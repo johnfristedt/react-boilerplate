5c8e515a6b745c71777c21ce10e2079b382126fd
John Fristedt <john@johnfristedt.com>
2016-08-21 11:51:23 +0200

    Initial commit

---
+++ src/js/containers/LandingPage/LandingPageApp.js
@@ -0,0 +1,14 @@
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+class LandingPageApp extends Component {
+
+  render () {
+    return(
+      <div>This is the landing page</div>
+    );
+  }
+}
+
+export default LandingPageApp;
--- src/js/routes.js
+++ src/js/routes.js
@@ -3,11 +3,12 @@ import { Route, IndexRoute, Redirect } from 'react-router';
 
 import App from './components/App';
 import FriendListApp from './containers/FriendListApp/FriendListApp';
+import LandingPageApp from './containers/LandingPage/LandingPageApp';
 import NotFoundView from './views/NotFoundView';
 
 export default (
   <Route path="/" component={App}>
-    <IndexRoute component={FriendListApp} />
+    <IndexRoute component={LandingPageApp} />
     <Route path="404" component={NotFoundView} />
     <Redirect from="*" to="404" />
   </Route>
